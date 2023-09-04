89d9e2d5ba9c2721578e860f6ace55b6bbf4b05c
JT Smith <nexxai@gmail.com>
2023-09-04 14:02:06 -0600

    Added generateRandomString function

--- express_server.js
+++ express_server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const uuid = require("uuid");
 const PORT = 8080; // default port 8080
+
+const generateRandomString = function () {
+  return uuid.v4().substring(0, 6);
+};
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -8,6 +13,13 @@ const urlDatabase = {
 };
 
 app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
