c6452e934637bc475b8b9782f8fd90e2f4b83abf
JT Smith <nexxai@gmail.com>
2023-09-05 13:40:10 -0600

    Added login functionality

--- express_server.js
+++ express_server.js
@@ -68,6 +68,11 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
